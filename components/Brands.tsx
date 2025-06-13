0d08e200ded3e08150312c00dc827ed94550f91d
Ivan Maidana <ivan.maidana99122@gmail.com>
2025-06-13 00:00:24 -0300

    pushing proy

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -16,14 +16,14 @@ const logos = [
 export default function Brands() {
   return (
     <section id="marcas" className="py-16 bg-gray-100">
-      <div className="mx-auto px-4 lg:px-0 overflow-hidden">
+      <div className="mx-auto px-4 sm:px-8 lg:px-0 overflow-hidden">
         <div
-          className="flex items-center space-x-12 w-max"
+          className="flex items-center space-x-6 sm:space-x-8 md:space-x-12 w-max"
           style={{ animation: 'scroll 250s linear infinite' }}
         >
           {/* Duplicamos el array para loop continuo */}
           {[...logos, ...logos].map((name, idx) => (
-            <div key={idx} className="flex-shrink-0 w-52 h-28 relative">
+            <div key={idx} className="flex-shrink-0 relative w-32 sm:w-40 md:w-52 h-16 sm:h-20 md:h-28 ">
               <Image
                 src={`/images/brands/${name}.png`}
                 alt={name}
