ca8e280e1488292fa6504cd8b7b5233c05092001
Ivan Maidana <ivan.maidana99122@gmail.com>
2025-06-18 02:37:08 -0300

    page finish 03 - paginative - fix refresh navigation

--- components/ClientAOS.tsx
+++ components/ClientAOS.tsx
@@ -1,29 +1,15 @@
-'use client'
-
+'use client';
+import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 export default function ClientAOS() {
+  const path = usePathname();
   useEffect(() => {
-    const initAOS = () => {
-      AOS.init({
-        duration: 800,
-        once: true,
-        disable: () => window.innerWidth < 640, // opcional, mantiene disable en móvil
-      });
-      AOS.refresh();
-    };
-
-    // Si ya se cargó la página
-    if (document.readyState === 'complete') {
-      initAOS();
-    } else {
-      // Espera al evento load
-      window.addEventListener('load', initAOS);
-      return () => window.removeEventListener('load', initAOS);
-    }
-  }, []);
+    AOS.init({ duration: 800, once: true, disable: () => window.innerWidth < 640 });
+    AOS.refresh();
+  }, [path]); // se volverá a inicializar en cada cambio de ruta o hash
 
   return null;
 }
