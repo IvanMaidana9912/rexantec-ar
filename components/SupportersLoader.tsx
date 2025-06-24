c8970f522d2f587eeed9ca229177803fbd32064f
Maidana Ivan <133286230+IvanMaidana9912@users.noreply.github.com>
2025-06-24 16:28:08 +0000

    fix?

--- components/SupportersLoader.tsx
+++ components/SupportersLoader.tsx
@@ -5,9 +5,11 @@ import dynamic from 'next/dynamic';
 // Importamos Supporters **solo en cliente**, sin SSR
 const Supporters = dynamic(
   () => import('../components/Supporters'),
-    { ssr: false }
+    { ssr: false ,
+      loading: () => (<div className='overflow-x-hidden h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px]
+      '/>)
+    }
     );
 
     export default function SupportersLoader() {
-      return <Supporters />;
-      };
+      return ( <div className="overflow-x-hidden"><Supporters /></div>);
