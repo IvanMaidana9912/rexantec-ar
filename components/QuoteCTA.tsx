deb82f1c3368bded58d6e229a2f84948e9a20b20
Ivan Maidana <ivan.maidana99122@gmail.com>
2025-06-29 22:56:13 -0300

    refactors (7)

--- components/QuoteCTA.tsx
+++ components/QuoteCTA.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
-import { FaWhatsapp } from 'react-icons/fa';
 import WppButtom from './WppButtom';
 
 export default function QuoteCTA() {
