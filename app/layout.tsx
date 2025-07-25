2af54fcef8302ae5077c15448e0474f264b837c9
Ivan Maidana <ivan.maidana99122@gmail.com>
2025-07-25 15:34:32 -0300

    add juani google

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,42 +45,38 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      //! Android Icons
-      { rel: "icon", type: "image/png", sizes: "36x36", url: "/favicon/android-icon-36x36.png", },
-      { rel: "icon", type: "image/png", sizes: "48x48", url: "/favicon/android-icon-48x48.png", },
-      { rel: "icon", type: "image/png", sizes: "72x72", url: "/favicon/android-icon-72x72.png", },
-      { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon/android-icon-96x96.png", },
-      { rel: "icon", type: "image/png", sizes: "144x144", url: "/favicon/android-icon-144x144.png", },
-      { rel: "icon", type: "image/png", sizes: "192x192", url: "/favicon/android-icon-192x192.png", },
-      { rel: "icon", type: "image/png", sizes: "512x512", url: "/favicon/android-icon-512x512.png", },
-
-      //! Apple Icons
-      { rel: "apple-touch-icon", type: "image/ico", url: "/favicon/apple-icon.png", },
-      { rel: "apple-touch-icon", sizes: "57x57", url: "/favicon/apple-icon-57x57.png", },
-      { rel: "apple-touch-icon", sizes: "60x60", url: "/favicon/apple-icon-60x60.png", },
-      { rel: "apple-touch-icon", sizes: "72x72", url: "/favicon/apple-icon-72x72.png", },
-      { rel: "apple-touch-icon", sizes: "76x76", url: "/favicon/apple-icon-76x76.png", },
-      { rel: "apple-touch-icon", sizes: "114x114", url: "/favicon/apple-icon-114x114.png", },
-      { rel: "apple-touch-icon", sizes: "120x120", url: "/favicon/apple-icon-120x120.png", },
-      { rel: "apple-touch-icon", sizes: "144x144", url: "/favicon/apple-icon-144x144.png", },
-      { rel: "apple-touch-icon", sizes: "152x152", url: "/favicon/apple-icon-152x152.png", },
-      { rel: "apple-touch-icon", sizes: "180x180", url: "/favicon/apple-icon-180x180.png", },
-
-      //! Favion Icons
-      { rel: "icon", type: "image/ico", url: "/favicon/favicon.ico", },
-      { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon/favicon-16x16.png", },
-      { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon/favicon-32x32.png", },
-      { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon/favicon-96x96.png", },
+      // Android Icons
+      { rel: 'icon', type: 'image/png', sizes: '36x36', url: '/favicon/android-icon-36x36.png' },
+      { rel: 'icon', type: 'image/png', sizes: '48x48', url: '/favicon/android-icon-48x48.png' },
+      { rel: 'icon', type: 'image/png', sizes: '72x72', url: '/favicon/android-icon-72x72.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', url: '/favicon/android-icon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '144x144', url: '/favicon/android-icon-144x144.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', url: '/favicon/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '512x512', url: '/favicon/android-icon-512x512.png' },
+      // Apple Icons
+      { rel: 'apple-touch-icon', type: 'image/ico', url: '/favicon/apple-icon.png' },
+      { rel: 'apple-touch-icon', sizes: '57x57', url: '/favicon/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', url: '/favicon/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', url: '/favicon/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', url: '/favicon/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', url: '/favicon/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', url: '/favicon/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', url: '/favicon/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', url: '/favicon/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', url: '/favicon/apple-icon-180x180.png' },
+      // Favicon Icons
+      { rel: 'icon', type: 'image/ico', url: '/favicon/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', url: '/favicon/favicon-96x96.png' },
     ],
-
-    //! Other Icons
-    other: [{ rel: 'apple-touch-icon-precomposed', url: '/favicon/apple-icon-precomposed.png', },],
+    other: [{ rel: 'apple-touch-icon-precomposed', url: '/favicon/apple-icon-precomposed.png' }],
   },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const updatedTimestamp = Math.floor(Date.now() / 1000);
 
-  const updatedTimestamp = Math.floor(Date.now() / 1000)
   return (
     <html lang="es-419">
       <head>
@@ -92,32 +88,53 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="me" href="https://m.facebook.com/100083022592038/" />
         <link rel="me" href="https://youtube.com/@rexantec-f8x" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17385788973" />
         <script
-          type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'https://schema.org',
-              '@type': 'Organization',
-              name: 'Rexantec',
-              url: 'https://www.rexantec.com.ar/',
-              logo: 'https://i.imgur.com/Cgf5Wnx.png',
-              sameAs: [
-                'https://m.facebook.com/100083022592038/',
-                'https://www.instagram.com/rexantec_servicios',
-                'https://youtube.com/@rexantec-f8x',
-                'mailto:rexantecservicios@gmail.com',
-                'https://api.whatsapp.com/send/?phone=5491158947428&text=Hola+Rexantec%2C+me+gustar%C3%ADa+solicitar+un+presupuesto+sobre%3A+%0AEstas+son+las+fotos+y%2Fo+videos%3A&type=phone_number&app_absent=0'
-              ],
-            }),
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}  
+              gtag('js', new Date());
+              gtag('config', 'AW-17385788973');
+            `,
+          }}/>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1G3S8YD5F8" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}  
+              gtag('js', new Date());
+              gtag('config', 'G-1G3S8YD5F8');`,
+          }}/>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'? '&l='+l: '';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WX4QZRV6');`,
           }}
         />
       </head>
       <body className="bg-[#0f446ca1] overflow-x-hidden select-none">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WX4QZRV6"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}/>
+        </noscript>
         <CatchAllErrors>
           <ClientAOS />
           {children}
         </CatchAllErrors>
       </body>
     </html>
-  )
-};
+  );
+}
